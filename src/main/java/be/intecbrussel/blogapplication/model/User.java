d1818942c2f3e0d69288e21846ac6645781f02e1
Yvonnick URVOY <yvonnick.urvoy@gmail.com>
2021-02-12 15:45:29 +0100

    "first commit"

--- src/main/java/be/intecbrussel/blogapplication/model/User.java
+++ src/main/java/be/intecbrussel/blogapplication/model/User.java
@@ -1,7 +1,16 @@
 package be.intecbrussel.blogapplication.model;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@Entity
 public class User {
+    @Id
     private Long id;
     private String email;
     private String password;
+
 }
