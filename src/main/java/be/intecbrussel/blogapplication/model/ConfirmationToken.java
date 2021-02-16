22778b52711d0092ce18982760bad416f561a445
Yvonnick URVOY <yvonnick.urvoy@gmail.com>
2021-02-16 15:23:57 +0100

    forgotPassword controller

--- src/main/java/be/intecbrussel/blogapplication/model/ConfirmationToken.java
+++ src/main/java/be/intecbrussel/blogapplication/model/ConfirmationToken.java
@@ -3,28 +3,35 @@ package be.intecbrussel.blogapplication.model;
 import lombok.Data;
 
 import javax.persistence.*;
-import java.time.LocalDate;
+import java.util.Date;
 import java.util.UUID;
 
 @Entity
 @Data
-class ConfirmationToken {
+@Table(name="confirmationToken")
+public class ConfirmationToken {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "token_id")
+    private long tokenid;
 
+    @Column(name = "confirmation_token")
     private String confirmationToken;
 
-    private LocalDate createdDate;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
 
     @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
     @JoinColumn(nullable = false, name = "user_id")
     private User user;
 
-    ConfirmationToken(User user) {
+    public ConfirmationToken() {
+    }
+
+    public ConfirmationToken(User user) {
         this.user = user;
-        this.createdDate = LocalDate.now();
-        this.confirmationToken = UUID.randomUUID().toString();
+        createdDate = new Date();
+        confirmationToken = UUID.randomUUID().toString();
     }
 }
--- src/main/java/be/intecbrussel/blogapplication/repositories/ConfirmationTokenRepository.java
+++ src/main/java/be/intecbrussel/blogapplication/repositories/ConfirmationTokenRepository.java
@@ -3,4 +3,4 @@ package be.intecbrussel.blogapplication.repositories;
 import be.intecbrussel.blogapplication.model.ConfirmationToken;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ConfirmationTokenRepository extends CrudRepository<ConfirmationToken, Long> { }
+public interface ConfirmationTokenRepository extends CrudRepository <ConfirmationToken, Long> { }
