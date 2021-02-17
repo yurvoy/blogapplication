ae576de259deb6e1740932ddcbeddcf806171b84
Yvonnick URVOY <yvonnick.urvoy@gmail.com>
2021-02-17 09:15:22 +0100

    forgotPassword.html

--- src/main/java/be/intecbrussel/blogapplication/web/WebSecurityConfig.java
+++ src/main/java/be/intecbrussel/blogapplication/web/WebSecurityConfig.java
@@ -24,6 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers(
                         "/registration**",
+                        "/forgotPassword**",
                         "/js/**",
                         "/css/**",
                         "/img/**",
