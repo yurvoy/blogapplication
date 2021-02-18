ba215a3f03f9b93043dcc34cf0e9d40448342f8c
Yvonnick URVOY <yvonnick.urvoy@gmail.com>
2021-02-18 21:31:46 +0100

    edit profile fix

--- src/main/java/be/intecbrussel/blogapplication/web/WebSecurityConfig.java
+++ src/main/java/be/intecbrussel/blogapplication/web/WebSecurityConfig.java
@@ -35,7 +35,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                         "/resetPassword**",
                         "/user/profile**",
                         "/user/updateProfile**",
-                        "/user/uploadImage**",
                         "/js/**",
                         "/css/**",
                         "/img/**",
