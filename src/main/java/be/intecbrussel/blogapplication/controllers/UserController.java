f1202c03e32daf8ed2c5291946ab8a132f157bfe
Yvonnick URVOY <yvonnick.urvoy@gmail.com>
2021-02-18 11:31:37 +0100

    errorController interface added

--- src/main/java/be/intecbrussel/blogapplication/controllers/UserController.java
+++ src/main/java/be/intecbrussel/blogapplication/controllers/UserController.java
@@ -2,6 +2,7 @@ package be.intecbrussel.blogapplication.controllers;
 
 import be.intecbrussel.blogapplication.model.User;
 import be.intecbrussel.blogapplication.repositories.UserRepository;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +12,7 @@ import java.security.Principal;
 
 
 @Controller
-public class UserController {
+public class UserController implements ErrorController {
 
 
     private final UserRepository userRepository;
@@ -40,6 +41,16 @@ public class UserController {
     @GetMapping("/user")
     public String userIndex() {
         return "user/index";
+    }
+
+    @RequestMapping("/error")
+    public String handleError() {
+        return "redirect:/";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return null;
     }
 
 
