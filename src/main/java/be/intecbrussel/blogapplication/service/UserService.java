7015243731c7629cac2da5c28f63b47ecdf07f45
JorisDDaems <jorisddaems@gmail.com>
2021-02-22 16:42:43 +0100

    refactoring / debugging

--- src/main/java/be/intecbrussel/blogapplication/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package be.intecbrussel.blogapplication.service;
-
-import be.intecbrussel.blogapplication.model.User;
-
-public interface UserService extends CrudService<User, Long>{
-    User findById(Long id);
-    User save(User user);
-
-}
