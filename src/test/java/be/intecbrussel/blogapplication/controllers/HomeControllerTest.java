65610ede5af748804c9b4cfa260c6ecd698ebef9
michaelarayabe <michaelseifuaraya@gmail.com>
2021-02-22 23:34:49 +0100

    initial test class

---
+++ src/test/java/be/intecbrussel/blogapplication/controllers/HomeControllerTest.java
@@ -0,0 +1,33 @@
+package be.intecbrussel.blogapplication.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HomeControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void root() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void userIndex() {
+    }
+
+    @Test
+    void handleError() {
+    }
+
+    @Test
+    void getErrorPath() {
+    }
+}
