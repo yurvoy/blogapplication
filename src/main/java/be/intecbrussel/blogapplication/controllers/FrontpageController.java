508c00f407ba27dae4b4301cf05c6e67318cbf58
JorisDDaems <jorisddaems@gmail.com>
2021-02-23 16:23:06 +0100

    frontpage.html big layout still some errors

--- src/main/java/be/intecbrussel/blogapplication/controllers/FrontpageController.java
+++ src/main/java/be/intecbrussel/blogapplication/controllers/FrontpageController.java
@@ -3,6 +3,7 @@ package be.intecbrussel.blogapplication.controllers;
 import be.intecbrussel.blogapplication.model.User;
 import be.intecbrussel.blogapplication.service.PostService;
 import be.intecbrussel.blogapplication.services.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,8 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
 
+@Slf4j
 @Controller
-public class FrontpageController implements ErrorController {
+public class FrontpageController {
+
+    private static final String USER_FRONTPAGE_URL = "user/frontpage";
 
     private final UserService userService;
     private final PostService postService;
@@ -24,28 +28,26 @@ public class FrontpageController implements ErrorController {
         this.postService = postService;
     }
 
-    @GetMapping("/user/frontpage")
-    public String showFrontPage(@PathVariable Long userId, Model model) {
 
-        User user = userService.findById(userId);
-        model.addAttribute("view", "user/frontpage");
-        model.addAttribute("user", user);
+    @GetMapping("/index/frontpage")
+    public String root(Model model) {
+
+        log.debug("getting frontpage");
+
+
+        return "user/frontpage";
+    }
+
+
+
+    @GetMapping({"/user/{userId}/frontpage"})
+    public String showUserFrontPage(@PathVariable Long userId, Model model) {
+
+        //model.addAttribute("view", "user/frontpage");
+        model.addAttribute("user", userService.findById(userId));
         return "/user/frontpage";
     }
 
-    @RequestMapping("/frontpage")
-    public String root(Principal principal, Model model) {
-
-        if (principal == null) {
-            return "login";
-        }
-        User user = userService.findByEmail(principal.getName());
-
-        model.addAttribute("user", user);
-
-
-        return "frontpage";
-    }
 
     @GetMapping({"frontpage/login"})
     public String login(Model model) {
@@ -59,12 +61,27 @@ public class FrontpageController implements ErrorController {
 
     @RequestMapping("frontpage/error")
     public String handleError() {
-        return "redirect:/";
+        return "redirect:/user/frontpage";
     }
 
+    /*
     @Override
     public String getErrorPath() {
         return null;
+    }
+
+     */
+
+    @RequestMapping({"/frontpage"})
+    public String root(Principal principal, Model model) {
+        if (principal == null){
+            return "redirect: /user/frontpage";
+        }
+        User user = userService.findByEmail(principal.getName());
+
+        model.addAttribute("user", user);
+
+        return "redirect:/user/frontpage";
     }
 
 
