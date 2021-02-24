f927a903dc979139f37823938232d5114f3444e8
michaelarayabe <michaelseifuaraya@gmail.com>
2021-02-24 12:38:07 +0100

    small fixs

--- src/main/java/be/intecbrussel/blogapplication/builder/UserBuilder.java
+++ src/main/java/be/intecbrussel/blogapplication/builder/UserBuilder.java
@@ -1,16 +1,8 @@
 package be.intecbrussel.blogapplication.builder;
 
-<<<<<<< HEAD
-import be.intecbrussel.blogapplication.model.Role;
 import be.intecbrussel.blogapplication.model.User;
 
 import java.time.LocalDate;
-import java.util.Collection;
-=======
-import be.intecbrussel.blogapplication.model.User;
-
-import java.time.LocalDate;
->>>>>>> forgotPassword
 
 public class UserBuilder {
 
