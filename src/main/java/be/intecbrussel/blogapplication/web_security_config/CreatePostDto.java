d1304c9c476759e2b6939962cbae56b0df404c0d
Yvonnick URVOY <yvonnick.urvoy@gmail.com>
2021-02-26 12:22:17 +0100

    empty post error fix

--- src/main/java/be/intecbrussel/blogapplication/web_security_config/CreatePostDto.java
+++ src/main/java/be/intecbrussel/blogapplication/web_security_config/CreatePostDto.java
@@ -7,11 +7,10 @@ import javax.validation.constraints.Size;
 @Data
 public class CreatePostDto {
 
-    @Size(max = 70, min = 1, message = "50 max characters")
+    @Size(max = 70, min = 1, message = "Title : 70 max characters")
     private String postTitle;
 
-    @Size(max = 500, min = 1, message = "400 max characters")
+    @Size(max = 500, min = 1, message = "Post : 500 max characters")
     private String postText;
-
 
 }
