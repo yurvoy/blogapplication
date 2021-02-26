461574a6018fba4e76629f1a217afcdcd86b814c
Yvonnick URVOY <yvonnick.urvoy@gmail.com>
2021-02-26 18:35:18 +0100

    registration error fixed

--- src/main/java/be/intecbrussel/blogapplication/web_security_config/UserRegistrationDto.java
+++ src/main/java/be/intecbrussel/blogapplication/web_security_config/UserRegistrationDto.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @FieldMatch.List({
         @FieldMatch(first = "password", second = "confirmPassword", message = "The password fields must match")
