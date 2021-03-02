1669aeced532ca7e17e30847f8ba51386f72484d
Jesus Bueno <jessbuenos@gmail.com>
2021-03-02 16:26:17 +0100

    jquery added and dropdown buttons working

--- src/main/java/be/intecbrussel/blogapplication/controllers/ImageController.java
+++ src/main/java/be/intecbrussel/blogapplication/controllers/ImageController.java
@@ -6,10 +6,7 @@ import be.intecbrussel.blogapplication.services.UserService;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -49,6 +46,7 @@ public class ImageController {
     }
 
     @GetMapping("user/{id}/profileimage")
+    @ResponseBody
     public void renderImageFromDB(@PathVariable String id, HttpServletResponse response) throws IOException {
         User user = userService.findById(Long.valueOf(id));
 
