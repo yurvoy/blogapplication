7f1564b1f84ad3e29a5cfe58f290c7956e658cab
Yvonnick URVOY <yvonnick.urvoy@gmail.com>
2021-03-08 13:17:01 +0100

    deploy config test14

--- src/main/java/be/intecbrussel/blogapplication/web_security_config/WebConfig.java
+++ src/main/java/be/intecbrussel/blogapplication/web_security_config/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -13,11 +14,16 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @ComponentScan
 public class WebConfig implements WebMvcConfigurer {
 
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/user").setViewName("redirect:/user/");
+        registry.addViewController("/user/").setViewName("forward:/user/frontpage.html");
+    }
+
     public ViewResolver viewResolver()
     {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
         viewResolver.setPrefix("src/main/resources/templates");
-        viewResolver.setPrefix("src/main/resources/templates/user");
         viewResolver.setSuffix(".html");
         viewResolver.setOrder(0);
         viewResolver.setExposeContextBeansAsAttributes(true);
