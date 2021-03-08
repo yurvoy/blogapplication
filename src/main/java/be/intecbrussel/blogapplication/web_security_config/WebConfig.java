2f16d0a6cb102f35b45d65dbceae94841830a46a
Yvonnick URVOY <yvonnick.urvoy@gmail.com>
2021-03-08 13:21:47 +0100

    deploy config test15

--- src/main/java/be/intecbrussel/blogapplication/web_security_config/WebConfig.java
+++ src/main/java/be/intecbrussel/blogapplication/web_security_config/WebConfig.java
@@ -14,16 +14,16 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @ComponentScan
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/user").setViewName("redirect:/user/");
-        registry.addViewController("/user/").setViewName("forward:/user/frontpage.html");
-    }
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("/user").setViewName("redirect:/user/");
+//        registry.addViewController("/user/").setViewName("forward:/user/frontpage.html");
+//    }
 
     public ViewResolver viewResolver()
     {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix("src/main/resources/templates");
+        viewResolver.setPrefix("src/main/resources/templates**/*");
         viewResolver.setSuffix(".html");
         viewResolver.setOrder(0);
         viewResolver.setExposeContextBeansAsAttributes(true);
