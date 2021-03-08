cffe5a7216dbb4ab410187f3314641e8343655b6
Yvonnick URVOY <yvonnick.urvoy@gmail.com>
2021-03-08 15:57:42 +0100

    deploy config test23

--- src/main/java/be/intecbrussel/blogapplication/web_security_config/WebSecurityConfig.java
+++ src/main/java/be/intecbrussel/blogapplication/web_security_config/WebSecurityConfig.java
@@ -23,7 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()
+        http.authorizeRequests()
                 .antMatchers(
                         "/**",
                         "/user/frontpage",
