71bb9d1f8b0563e3638c3f5820120b61d89456b0
Yvonnick URVOY <yvonnick.urvoy@gmail.com>
2021-03-08 14:04:47 +0100

    deploy config test20

--- src/main/java/be/intecbrussel/blogapplication/web_security_config/WebConfig.java
+++ src/main/java/be/intecbrussel/blogapplication/web_security_config/WebConfig.java
@@ -6,9 +6,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @EnableWebMvc
 @Configuration
@@ -23,5 +24,18 @@ public class WebConfig implements WebMvcConfigurer {
         viewResolver.setOrder(0);
         viewResolver.setExposeContextBeansAsAttributes(true);
         return viewResolver;
+    }
+
+    @Bean
+    public ClassLoaderTemplateResolver secondaryTemplateResolver() {
+        ClassLoaderTemplateResolver secondaryTemplateResolver = new ClassLoaderTemplateResolver();
+        secondaryTemplateResolver.setPrefix("src/main/resources/templates/user");
+        secondaryTemplateResolver.setSuffix(".html");
+        secondaryTemplateResolver.setTemplateMode(TemplateMode.HTML);
+        secondaryTemplateResolver.setCharacterEncoding("UTF-8");
+        secondaryTemplateResolver.setOrder(1);
+        secondaryTemplateResolver.setCheckExistence(true);
+
+        return secondaryTemplateResolver;
     }
 }
