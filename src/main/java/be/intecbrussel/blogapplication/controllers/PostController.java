4de535fadcae0a3b68c864afe3a4109678b8bdec
JorisDDaems <jorisddaems@gmail.com>
2021-03-08 13:47:07 +0100

    Delete button gets redirected to profile page

--- src/main/java/be/intecbrussel/blogapplication/controllers/PostController.java
+++ src/main/java/be/intecbrussel/blogapplication/controllers/PostController.java
@@ -31,7 +31,7 @@ public class PostController {
     }
 
     @GetMapping("user/{userId}/createPost")
-    public String showUploadForm(@PathVariable String userId, Model model){
+    public String showUploadForm(@PathVariable String userId, Model model) {
 
         User existing = userService.findById(Long.parseLong(userId));
         if (existing == null) {
@@ -56,39 +56,71 @@ public class PostController {
     }
 
     @GetMapping("editPost/{id}")
-    public String editPost(@PathVariable Long id, Model model, Principal principal){
+    public String editPost(@PathVariable Long id, Model model, Principal principal) {
 
         String user = "aUser";
 
-        if(principal != null){
+        if (principal != null) {
             user = principal.getName();
         }
 
         Post post = this.postService.findById(id);
 
-        if(post != null){
+        if (post != null) {
 
-            if(user.equals(post.getUser().getEmail())){
+            if (user.equals(post.getUser().getEmail())) {
                 model.addAttribute("post", post);
                 return "user/updatePost";
-            }else{
+            } else {
                 return "403";
             }
-        }else{
+        } else {
             return "error";
         }
 
     }
 
     @PostMapping("editPost/{id}")
-    public String processUpdatePost(@PathVariable Long id, Principal principal, @ModelAttribute("post") CreatePostDto postForm){
+    public String processUpdatePost(@PathVariable Long id, Principal principal, @ModelAttribute("post") CreatePostDto postForm) {
 
         postService.updatePost(id, principal, postForm);
         return "redirect:/user/" + postService.findById(id).getUser().getId() + "/profile";
     }
 
+    @GetMapping("deletePost/{id}")
+    public String deletePost(@PathVariable Long id, Model model, Principal principal) {
+
+        String user = "aUser";
+
+        if (principal != null) {
+            user = principal.getName();
+        }
+
+        Post post = this.postService.findById(id);
+
+        if (post != null) {
+
+            if (user.equals(post.getUser().getEmail())) {
+                model.addAttribute("post", post);
+                return "user/deletePost";
+            } else {
+                return "403";
+            }
+        } else {
+            return "error";
+        }
+    }
+
+    @PostMapping("deletePost/{id}")
+    public String processDeletePost(@PathVariable Long id, Long postId) {
+
+        postService.deleteById(id, postId);
+        return "redirect:/user/" + postService.findById(id).getUser().getId() + "/profile";
+    }
+
+
     @PostMapping("/error")
-    public String error(){
+    public String error() {
         return "redirect:/";
     }
 }
