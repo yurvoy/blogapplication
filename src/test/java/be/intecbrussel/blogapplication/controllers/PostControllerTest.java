5ba41087484def2bf9508d6965e90d61dd4f5749
Jesus Bueno <jessbuenos@gmail.com>
2021-03-17 08:37:22 +0100

    PostControllerTest: reviewPosts,CreatePostfromProfile and from Frontrpage tests added

---
+++ src/test/java/be/intecbrussel/blogapplication/controllers/PostControllerTest.java
@@ -0,0 +1,123 @@
+package be.intecbrussel.blogapplication.controllers;
+
+import be.intecbrussel.blogapplication.model.Post;
+import be.intecbrussel.blogapplication.model.User;
+import be.intecbrussel.blogapplication.services.PostService;
+import be.intecbrussel.blogapplication.services.UserService;
+import be.intecbrussel.blogapplication.web_security_config.WebConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.security.Principal;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+
+@ExtendWith(MockitoExtension.class)
+public class PostControllerTest {
+
+    @InjectMocks
+    WebConfig webConfig;
+
+    @Mock
+    UserService userService;
+
+    @Mock
+    PostService postService;
+
+    @InjectMocks
+    PostController postController;
+
+    MockMvc mockMvc;
+
+    User user;
+
+    Post post;
+
+    Principal mockPrincipal;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        mockPrincipal = mock(Principal.class);
+        when(mockPrincipal.getName()).thenReturn("abc@gmail.com");
+
+        postController = new PostController(userService, postService);
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(postController)
+                .setViewResolvers(webConfig.viewResolver())
+                .build();
+
+        post = new Post();
+        user = new User();
+        user.setId(1L);
+        post.setId(1L);
+        post.setUser(user);
+    }
+
+
+    @Test
+    public void createNewPostFromProfile() throws Exception{
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/user/" + user.getId() + "/createPost/profile")
+                .principal(mockPrincipal);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(view().name("redirect:/user/" + user.getId() + "/profile"));
+    }
+
+    @Test
+    public void createNewPostFromFrontPage() throws Exception{
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/user/" + user.getId() + "/createPost/frontpage")
+                .principal(mockPrincipal);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(view().name("redirect:/"));
+    }
+
+
+    @Test
+    public void editPost() throws Exception {
+        when(postService.findById(anyLong())).thenReturn(post);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/editPost/" + post.getId())
+                .principal(mockPrincipal);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(view().name("redirect:/index"));
+    }
+
+    @Test
+    public void reviewPosts() throws Exception{
+
+        when(userService.findById(1L)).thenReturn(user);
+        when(userService.findByEmail(any())).thenReturn(user);
+
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/user/" + user.getId() + "/reviewPosts")
+                .principal(mockPrincipal);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(view().name("user/configuration/reviewPosts"));
+    }
+
+
+}
