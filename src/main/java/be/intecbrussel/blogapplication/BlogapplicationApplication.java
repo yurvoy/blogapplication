fcb138de32a50d0580d9b1c496377b28c20aacb1
michaelarayabe <michaelseifuaraya@gmail.com>
2021-03-28 14:51:23 +0200

    security configuration going public issues fixed

--- src/main/java/be/intecbrussel/blogapplication/BlogapplicationApplication.java
+++ src/main/java/be/intecbrussel/blogapplication/BlogapplicationApplication.java
@@ -2,7 +2,10 @@ package be.intecbrussel.blogapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
+@PropertySource(value = {"file:config/application-dev.properties", "file:config/application.properties", "file:config/application-prod.properties"})
 @SpringBootApplication
 public class BlogapplicationApplication {
 
